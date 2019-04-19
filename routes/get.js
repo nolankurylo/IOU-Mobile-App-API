dc61de465045559cc18f629d165efaa7c13c2ab5
Nolan <kurylon@telus.net>
2019-04-19 16:07:03 -0700

    get

--- routes/get.js
+++ routes/get.js
@@ -67,7 +67,7 @@ router.get("/get_friends/:user_id", async (req, res) => {
   });
 });
 
-router.post("/get_friend_requests/:user_id", (req, res) => {
+router.get("/get_friend_requests/:user_id", (req, res) => {
   values = [req.params.user_id]
   text = `SELECT * FROM friends WHERE (user_a_id = $1 AND status ='req') OR (user_b_id = $1 AND status ='req')`
   query(text, values, (err, result) => {
