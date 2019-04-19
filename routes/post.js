a7225074f678dd4434ecc2b0375cbf9bb8db1b54
Nolan <kurylon@telus.net>
2019-04-19 14:51:25 -0700

    get_users moved to post

--- routes/post.js
+++ routes/post.js
@@ -181,4 +181,17 @@ router.post("/cancel_friend_request", async (req, res) => {
     return res.status(200).send({ success: true });
   });
 })
+
+router.post("/get_users", async (req, res) => {
+  values = [req.body.user_id];
+ text = `WITH A AS (SELECT * FROM friends WHERE user_a_id = $1 OR user_b_id = $1)
+  SELECT * FROM account LEFT JOIN A ON account.id = A.user_b_id OR account.id = A.user_a_id WHERE id != $1`;
+  query(text, values, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({ error: "There was an internal error" });
+    }
+    return res.status(200).send({ users: result.rows });
+  });
+});
 module.exports = router;
