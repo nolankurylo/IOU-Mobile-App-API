c23d437e86a8bdaf53dbe7c80fbb52e6599411c8
Nolan <kurylon@telus.net>
2019-05-09 21:24:29 -0700

    fixed prev route

--- routes/post.js
+++ routes/post.js
@@ -292,7 +292,7 @@ router.post("/add_money_iou", (req, res) => {
   text = `BEGIN; `
   for (var i = 0; i < users.length; i++){
     text += `UPDATE houses SET amount = amount + `+ amount + `WHERE user_id = ` + curr_user + ` AND other_user = ` + users[i] + `;`
-    text += `UPDATE houses SET amount = amount - `+ amount + `WHERE user_id = ` + user[i] + ` AND other_user = ` + curr_user + `;`
+    text += `UPDATE houses SET amount = amount - `+ amount + `WHERE user_id = ` + users[i] + ` AND other_user = ` + curr_user + `;`
   }
   text += `END;`
   console.log(text)
