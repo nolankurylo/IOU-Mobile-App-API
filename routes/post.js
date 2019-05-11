b290e7787b90e926f6b95543c89e28449f46c10d
Nolan <kurylon@telus.net>
2019-05-10 17:41:06 -0700

    quotes

--- routes/post.js
+++ routes/post.js
@@ -296,7 +296,7 @@ router.post("/add_money_iou", (req, res) => {
   for (var i = 0; i < users.length; i++){
     text += `UPDATE houses SET amount = amount + `+ amount.toString() + ` WHERE user_id = ` + curr_user + ` AND other_user = ` + users[i] + `; `
     text += `UPDATE houses SET amount = amount - `+ amount.toString() + ` WHERE user_id = ` + users[i] + ` AND other_user = ` + curr_user + `; `
-    text += `insert into ious (user_id, other_user, house_id, amount, description) values (` + curr_user + `, ` + users[i] + `, ` + req.body.house_id + `, ` + amount.toString() + `, ` + req.body.description + `); `
+    text += `insert into ious (user_id, other_user, house_id, amount, description) values (` + curr_user + `, ` + users[i] + `, ` + req.body.house_id + `, ` + amount.toString() + `, '` + req.body.description + `'); `
   }
   text += `END;`
   values = []
